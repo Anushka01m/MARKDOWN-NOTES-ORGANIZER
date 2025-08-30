b4428b6aba67379ea9a2b973244f7e706c03797f
Anushka Maitra <anushka.mukti3011@gmail.com>
2025-08-31 01:01:41 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,1 +1,27 @@
+from flask import Flask
+from flask_login import LoginManager
+from models import db, User
+from routes import routes
+from flask_socketio import SocketIO
+
+app = Flask(__name__)
+app.config['SECRET_KEY'] = 'your-secret-key'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///notes.db'
+db.init_app(app)
+
+login_manager = LoginManager()
+login_manager.init_app(app)
+
+socketio = SocketIO(app)
+
+@login_manager.user_loader
+def load_user(user_id):
+    return User.query.get(int(user_id))
+
+app.register_blueprint(routes)
+
+if __name__ == "__main__":
+    with app.app_context():
+        db.create_all()
+    socketio.run(app, debug=True)
 
