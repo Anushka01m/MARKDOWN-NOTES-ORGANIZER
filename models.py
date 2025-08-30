4c8d64297596d4e2747a05fba58002469b1da20e
Anushka Maitra <anushka.mukti3011@gmail.com>
2025-08-31 01:02:12 +0530

    Update models.py

--- models.py
+++ models.py
@@ -1,1 +1,22 @@
+from flask_sqlalchemy import SQLAlchemy
+from flask_login import UserMixin
+from datetime import datetime
+
+db = SQLAlchemy()
+
+class User(db.Model, UserMixin):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(80), unique=True)
+    password_hash = db.Column(db.String(128))
+
+class Note(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
+    title = db.Column(db.String(120))
+    content = db.Column(db.Text)
+    tags = db.Column(db.String(120))
+    created_at = db.Column(db.DateTime, default=datetime.utcnow)
+    updated_at = db.Column(db.DateTime, default=datetime.utcnow)
+    vault = db.Column(db.String(80))
+    github_path = db.Column(db.String(200))
 
